f410791540451a2d7fdd5dd475ce4508d047df8a
omurertugrul61 <omurertugrul61@gmail.com>
2022-02-22 15:36:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const {MessageEmbed}=require("discord.js");
+const {Intents}=Discord;
+const client = new Discord.Client({
+"intents":[Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES]
+})
+
+
+client.once("ready",()=> {
+	console.log("selaminelykum");
+	client.user.setPresence({status:"dnd",activities:[{name:"alpin annesiyle",type:"PLAYING"}]})
+})
+
+client.on("messageCreate",msg=>{
+	if(msg.content.toLowerCase().includes("neden")){
+		msg.reply("ananin amindan dolayi")
+	}	
+})
+client.on("messageCreate",msg=>{
+	if(msg.content.toLowerCase().includes("nerde")){
+		msg.reply("ananin aminda")
+	}	
+})
+client.on("messageCreate",msg=>{
+	if(msg.content.toLowerCase().includes("nerede")){
+		msg.reply("ananin aminda")
+	}	
+})
+client.on("messageCreate",msg=>{
+	if(msg.content.toLowerCase().includes("gibi")){
+		msg.reply("seni siktigim gibymi")
+	}	
+})
+client.on("messageCreate",msg=>{
+	if(msg.content.toLowerCase().includes("sa")){
+		msg.reply("cami mi burası orospu evladı")
+	}	
+})
+
+client.login("OTQ1MzA1NjI2NTkzMDc5MzM2.YhOOeA.dK9TwbyX9jsi7_LKNVsDwtGFS5I");
